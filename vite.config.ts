d443645bbbab59ca5ec4adb10a36a7dfb6e84f1d
takamatsu-hideomi <h_takamatsu02@kost.jp>
2025-02-27 08:32:08 +0900

    カバレッジの対象ファイルをチューニング

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,13 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // ビルド設定
-  },
-  server: {
-    // サーバー設定
-  },
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: ["./vitest-setup.ts"],
+    coverage: {
+      all: false,
+      exclude: [],
+    },
   },
 });
