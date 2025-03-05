a3f64da81550382303d321dd3bde2e5e63aa51a4
takamatsu-hideomi <h_takamatsu02@kost.jp>
2025-03-05 13:45:12 +0900

    storybook追加

---
+++ src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Button } from ".";
+
+const meta: Meta<typeof Button> = {
+  title: "components/atoms/Button",
+  component: Button,
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  render: (args) => <Button {...args} />,
+  args: {
+    children: "hogehoge",
+  },
+};
+
+export const Small: Story = {
+  render: (args) => <Button {...args} />,
+  args: {
+    children: "hogehoge",
+    size: "small",
+  },
+};
