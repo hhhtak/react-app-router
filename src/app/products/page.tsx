bfe8374c3bc33959260c8be5673a720d0e1a3517
takamatsu-hideomi <h_takamatsu02@kost.jp>
2025-03-18 13:27:43 +0900

    apiの返り値を修正

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,7 +5,7 @@ export default async function ProductsPage() {
   // todo:開発したAPIなら型を縛ったほうがよさそう
   const zip = await fetch("https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060");
   const zipJson = await zip.json();
-  const data: Address[] = zipJson.result;
+  const data: Address[] = zipJson.results;
 
   return <Addresses data={data} />;
 }
