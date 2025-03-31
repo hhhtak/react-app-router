ba61db53ead252952382a185a45339a02c375f82
hhhtak <hhhtak@users.noreply.github.com>
2025-03-31 12:00:04 +0900

    UpdateNameFormのstorybook追加

---
+++ src/components/pages/UpdateNameForm/index.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from "@storybook/react";
+import UpdateNameForm from ".";
+
+const meta: Meta<typeof UpdateNameForm> = {
+  title: "components/pages/UpdateNameForm",
+  component: UpdateNameForm,
+};
+export default meta;
+
+type Story = StoryObj<typeof UpdateNameForm>;
+
+export const Default: Story = {
+  render: () => <UpdateNameForm />,
+};
